61c044de57422e7523a61709cd23748e110cb10e
Victor Lucas <victorlucas.ao@gmail.com>
2024-07-24 14:57:00 -0400

    feat: usando ConfigModule no Nest.js

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common'
-import { PrismaService } from './prisma/prisma.service'
+import { ConfigModule } from '@nestjs/config'
 import { CreateAccountController } from './controllers/create-account.controller'
+import { envSchema } from './env'
+import { PrismaService } from './prisma/prisma.service'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
